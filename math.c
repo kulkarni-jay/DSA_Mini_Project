71635ffda89928450622aaad50c63b331ae317da
kulkarni-jay <kulkarni.j12@gmail.com>
2016-11-02 18:23:23 +0530

    Add files via upload

--- math.c
+++ math.c
@@ -1,15 +1,43 @@
+/***************************************************************************** 
+ * Copyright (C) Jay Kulkarni
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include <stdio.h>
 #include "math.h"
 #include <stdlib.h>
 #include <errno.h>
+
+int check(double x) {
+	if(isnan(x) == 1)
+		return 1;
+	else if(isinf(x) == 1)
+		return 2;
+}
+
 double sine(double x) {
 	if(x == 0)
 		return 0;
-	else if(isnan(x) == 1) {
+	else if(check(x) == 1) {
+		errno = EDOM;
 		return NAN;
 	}
-	else if(isinf(x) == 1) {
-		return INFINITY;
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
 	}
 	else {
 		int flag = 0;
@@ -17,19 +45,16 @@ double sine(double x) {
 			flag = 1;
 			x = -x;
 		}
-		if(fabse(x) > M_PI_2) { /* term reduction */
-			while(fabse(x) > 2 * M_PI) {
-				long double y;
-				y = fmode(x, 2 * M_PI);
-				x = y;
-			}
+		if(fabse(x) > 2 * M_PI) { 			/* term reduction */
+			long double y = fmode(x, 2 * M_PI);
+			x = y;
 		}
 		int n;
 		long double sum, term;
 		n = 3;
 		sum = x;
 		term = x;
-		while(fabse(term) > 0.0000000000001) {
+		while(fabse(term) > 0.000000000001) {
 			term = ((-1) * term * (x * x)) / (n * (n - 1));
 			sum += term;
 			n = n + 2;		
@@ -41,16 +66,16 @@ double sine(double x) {
 }
 
 double sinhe(double x) {
-	if(fabse(x) > DBL_MAX)
-		return INFINITY;
-	if(isnan(x) == 1 || isinf(x) == 1) {
-		if(isnan(x))
-			return NAN;
-		if(isinf(x))
-			return INFINITY;
-	}
 	if(x == 0)
-		return 0;	
+		return 0;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}	
 	double term = x, sum = x;
 	int n = 1000000, i;
 	for(i = 3; i < n;) {
@@ -58,100 +83,129 @@ double sinhe(double x) {
 		sum += term;
 		i = i + 2;
 	}
-	return sum;
-	if(fabse(sum) > DBL_MAX)	{
+	if(fabse(sum) > DBL_MAX) {
 		errno = ERANGE;
 		return INFINITY;
 	}
+	return sum;
 }
+
 double asine(double x) {
-	int errno, i;
-	long double num, deno, term, sum;
+	
 	if(x > 1. || x < -1.) {
 		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
 	}
 	else if(x == 1)
 		return M_PI_2;
 	else if(x == -1)
-		return -M_PI_2;	
+		return -M_PI_2;
 	else {
-		num = 1;
-		deno = 2;
-		sum = x;
-		for(i = 2; i < 100 ;i++) {
-			if(i == 2)
-				num = num * (2*i - 1) * x * x * x;
-			else
-				num = num * (2*i - 1) * x * x;	
-			deno = deno * (2*i);
-			term = num / deno;
-			sum += term;
-		}
-		return sum;
-	}
-}	
-double cose(double x) {
-	if(x == 0)
-		return 1;
-	else if(isnan(x) == 1) {
-		return NAN;
-	}
-	else if(isinf(x) == 1) {
-		return INFINITY;
-	}
-	else {
+		int  i = 3;
 		int flag = 0;
-		if(x < 0)
-			x = -x;		/* cos(-x) = cos(x) */ 
-		if(fabse(x) > M_PI_2) { /* term reduction*/
-			while(fabse(x) > 2 * M_PI) {
-				long double y;
-				y = fmode(x, 2 * M_PI);
-				x = y;
-			}
+		if(x < 0) {
+			flag = 1;
+			x = -x;
 		}
-		double sum = 1, term = 1;
-		int n = 1;
-		while(fabse(term) > 0.00000000000001) {
-			term = (-1) * (term * x * x) / (n * (n-1));
+		long double term = powe(x, 3) / 6, sum = x + (powe(x, 3) / 6);
+		while(term > 0.00000000001) { 
+			term = (term * x * x * powe(i, 2)) / ((i + 1) * (i + 2));
 			sum += term;
-			n = n + 2;
+			i += 2;
 		}
+		if(flag == 1)
+			return -sum;
 		return sum;
 	}
 }
+
+double cose(double x) {
+	if(x == 0)
+		return 1;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
+	}
+	return sine(M_PI_2 - x);
+}
+
 double acose(double x) {
-	if(x >= -1 && x <= 1)
+	if(x == 0)
+		return M_PI_2;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(x >= -1 && x <= 1) {
+		if(x < 0)
+			return M_PI - acose(-x);
 		return M_PI_2 - asine(x);
+	}
 	else {
 		errno = EDOM;
 		return NAN; 
 	}	 
 }
+
 double coshe(double x) {
-	double sum = 1, term = 1;
-	int n = 2;
-	while(term > 0.00000000001) {
-		term = (term * x * x) / (n * (n - 1));
-		sum += term;
-		n = n + 2;
-	}
-	return sum;
-}
-double tane(double x) {
-	if(fmode(x, M_PI) == 0)
-		return 0.;
-	else if(isnan(x) == 1) {
-		return NAN;
-	}
-	else if(isinf(x) == 1) {
-		return INFINITY;
-	}
-	else if(fmode(fabse(x), M_PI_2) == 0) {           /* domain error */
+	if(x == 0)
+		return 1.;
+	else if(check(x) == 1) {
 		errno = EDOM;
 		return NAN;
 	}
-	else if(fmode(x, M_PI_4) == 1 && fmode(fabse(x), M_PI_2) != 0)
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
+	else {
+		double sum = 1, term = 1;
+		int n = 2;
+		while(term > 0.0000000001) {
+			term = (term * x * x) / (n * (n - 1));
+			sum += term;
+			n = n + 2;
+		}
+		if(fabse(sum) > DBL_MAX) {
+		errno = ERANGE;
+		return INFINITY;
+		}
+		return sum;
+	}
+}
+
+double tane(double x) {
+	if(fmode(x, M_PI) == 0)
+		return 0.;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(fmode(fabse(x), M_PI_2) == 0) {           /* domain error */
+		errno = EDOM;
+		return INFINITY;
+	}
+	else if(fmode(x, M_PI_4) == 1. && fmode(fabse(x), M_PI_2) != 0.)
 		return 1.;
 	else {
 		int flag = 0;
@@ -159,40 +213,65 @@ double tane(double x) {
 			flag = 1;
 			x = -x;
 		}
-		while(fabse(x) > M_PI) {		/* term reduction */
-			long double y;
-			y = fmode(x, M_PI);
-			x = y;
-		}
 		if(flag == 1)
 			return (-1)*(sine(x) / cose(x));
 		return (sine(x) / cose(x));
 	}
 }
+
 double tanhe(double x) {
-	//error cases not handled.
+	if(fmode(x, M_PI) == 0)
+		return 0.;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		return 1.;
+	}
+	else if(x > 8.)
+		return 1;
 	return (sinhe(x) / coshe(x));
 }
+
 double atane(double x) {
-	int i;
-	long double sum = x, term = x;
-	for(i = 1; i < 100 ; i++) {
-		term = powe(x, (2*i -1)) / (2*i - 1);
-		if(i%2 == 0)
-			sum -= term;
-		else
-			sum += term;	
+	if(fmode(x, M_PI) == 0)
+		return 0.;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
 	}
-	return sum;
+	else if(check(x) == 2) {
+		return M_PI_2;
+	}
+	int i = 3;
+	long double sum = x, term = x;
+	if(fabse(x) <= 1.) {
+		while(i < 100) {		//x -x^3/3 + x^5/5 ...... range : [-pi, pi]
+			term = powe(x, i) / i;
+			if(i % 4 == 3)
+				sum = sum - term;
+			else if(i % 4 == 1) 
+				sum += term;
+			i += 2;
+		}
+		return sum;
+	}
+	
 }
+
 double atan2e(double y, double x) {
 	if(x == 0)
 		return NAN;
 	else if(y == 0)
 		return 0;
-	/*
-	else if(part)
-	*/
+	else if(check(x) == 1 || check(y) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(y) == 2) {
+		return M_PI_2 / 2;
+	}
 	else {
 		long double term = (fabse(y) / fabse(x));
 		if(x > 0 && y > 0) {		/*first quadrant*/
@@ -204,18 +283,26 @@ double atan2e(double y, double x) {
 		else if(y < 0 && x < 0) {	/*third quadrant*/
 			return -(M_PI - atane(term));
 		}
-		return	(M_PI - atane(term));			/*fourth quadrant*/
+		return	(M_PI - atane(term));	/*fourth quadrant*/
 	}
 }
 
 double powe(double x, double y) {
 	int sign = 0;
-	long term, result;
+	long double term, result;
 	if(x==0) 
 		if(y == 0)
 			return 1;
 		else 
 			return 	0.0;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
 	else if((int)y == y){
 		if(y == 0)
 			return 1.0;
@@ -243,11 +330,20 @@ double powe(double x, double y) {
 		return (expe(exp));
 	}
 }
+
 double expe(double x) {
 	int sign = 0, n =1;
-	double term, sum;
+	long double term, sum;
 	if(x == 0)
 		return 1;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
 	else {
 		if(x < 0) {
 			sign = 1;
@@ -266,53 +362,119 @@ double expe(double x) {
 	else 
 		return sum;	
 }
+
 double floore(double x) {
-	int i = (int)x; 
-	if (i > x) 
-		return (double)(--i);
-	else if(i < x)
-		return (double)(i);
-	else
-		return x;			
-}
-double ceile(double x) {
-	int i =(int)x;
-	if (i > x) 
-		return (double)(i);
-	else if(i < x)
-		return (double)(++i);
-	else
-		return x;
-}
-double fabse(double x) {
-	if(x >= 0)
-		return x;
-	else 
-		return -x;
-}
-double loge(double x) {
-	int errno;
-	if(x < 1) {
+	if(check(x) == 1) {
 		errno = EDOM;
 		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
+	else {
+		int i = (int)x; 
+		if (i > x) 
+			return (double)(--i);
+		else if(i < x)
+			return (double)(i);
+		else
+			return x;
+	}			
+}
+
+double ceile(double x) {
+	if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
+	else { 
+		int i =(int)x;
+		if (i > x) 
+			return (double)(i);
+		else if(i < x)
+			return (double)(++i);
+		else
+			return x;
+	}
+}
+
+double fabse(double x) {
+	if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
+	else if(x >= 0)
+		return x;
+	return -x;
+}
+
+double loge(double x) {
+	if(x < 0) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(x == 0) {
+		errno = EDOM;
+		return INFINITY;
+	}
+	else if(x == 1)
+		return 0.;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
 	}
 	else {	
 		long int i = 1;
 		long double sum = 0, term = 1;
-		while(term > 0.00000000001) {
-			term *= (1-(1/x));
-			sum += term/i;
-			i++;
+		if(x > 1.) {
+			while(term > 0.0000001) {
+				term *= (1-(1/x));
+				sum += term/i;
+				i++;
+			}
+			return sum;
 		}
-		return sum;
+		else { //x < 1 && x > 0
+			long double num = (x - 1);
+			while(i < 2500) {
+				term = powe(num, i) / i;
+				if(i % 2 == 1)
+					sum += term;
+				else 
+					sum = sum - term;
+				i++;
+			}
+			return sum;
+		}
 	}
 }
+
 double log10e(double x) {
-	int errno;
 	long double ans;
-	if(x < 1) {
+	if(x < 0) {
 		errno = EDOM;
 		return NAN;
+	}
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
 	}
 	else if(fmode(x, 10) == 0) {
 		double count = 0;
@@ -327,9 +489,22 @@ double log10e(double x) {
 		return ans;
 	}		
 }
+
 double fmode(double x, double y) {
 	if(y == 0)
 		return NAN;
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(y) == 2) {
+		if(isnan(x) != 0 || isinf(x) != 0)
+			return x;
+	}
 	else if(x == 0)
 		return 0;
 	else{
@@ -337,34 +512,65 @@ double fmode(double x, double y) {
 		return (x - quo * y); 
 	}
 }
+
 double sqrte(double x) {
 	if(x < 0)
 		return NAN;
+	else if(check(x) == 1) {
+		return NAN;
+		errno = EDOM;
+	}
+	else if(check(x) == 2) {
+		return INFINITY;
+		errno = EDOM;
+	}
 	else if(x == 0)
 		return 0.;
-	else {
-		
-	}
+	return powe(x, 0.5);
 }
+
 double ldexpe(double x, int exponent) {
+	if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
 	long double ans, exp;
 	exp = powe(2, exponent);
 	ans = x * exp;
 	return ans;
 }
+
 double modfe(double x, double *integer) {
+	if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return NAN;
+	}
 	double in = (int)x;
 	*integer = in;
 	return (x - *integer);
 }
+
 double frexpe(double x, int *exponent){
 	if(x == 0) {
 		*exponent = 0;
 		return 0;
 	}
-	/*
-	else if part
-	*/
+	else if(check(x) == 1) {
+		errno = EDOM;
+		return NAN;
+	}
+	else if(check(x) == 2) {
+		errno = EDOM;
+		return INFINITY;
+	}
 	else {
 		int i = 1, sign = 0;
 		if(x < 0) {
